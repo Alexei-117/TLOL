dc5a0008423c8e53cb5f265c8fe7031779c200fe
Alexei-117 <alexei-117@hotmail.com>
2017-08-02 21:17:24 +0200

    Initial commit

---
+++ Source/TLOL/TLOLGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TLOLGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TLOL_API ATLOLGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
